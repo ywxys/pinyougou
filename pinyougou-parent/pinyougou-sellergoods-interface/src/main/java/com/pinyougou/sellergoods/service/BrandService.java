3232199382e56e07aa7b5e5fe23239df41a592e9
itheima02 <504601742@qq.com>
2018-10-08 20:38:34 +0800

    第一天完

---
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,15 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌
+ * @author ywxys
+ *
+ */
+public interface BrandService {
+
+	public List<TbBrand> findAll();
+}

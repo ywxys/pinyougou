f76a206875d6f6a1e70ae71e05736a3e4d83abb2
itheima02 <504601742@qq.com>
2018-10-10 09:40:24 +0800

    第二天

--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌
  * @author ywxys
@@ -12,4 +14,45 @@ import com.pinyougou.pojo.TbBrand;
 public interface BrandService {
 
 	public List<TbBrand> findAll();
+	
+	/**
+	 * 品牌分页
+	 * @param pageNum	当前页码
+	 * @param pageSize	每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
+	
+	/**
+	 * 增加
+	 * @param brand
+	 */
+	public void add(TbBrand brand);
+	
+	/**
+	 * 根据id查询实体
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
+	
+	/**
+	 * 修改
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+	
+	/**
+	 * 删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+	
+	/**
+	 * 品牌分页
+	 * @param pageNum	当前页码
+	 * @param pageSize	每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
